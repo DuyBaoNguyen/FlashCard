3e06b0ed2280f51829aa7955f5e292378b7df56c
Duy Bao Nguyen <baobaotwd@gmail.com>
2019-12-25 00:44:39 +0700

    Add History on Dashboard

--- ClientApp/src/components/Users/Dashboard/Dashboard.js
+++ ClientApp/src/components/Users/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import authService from '../../api-authorization/AuthorizeService';
 import './Dashboard.css';
 import Navbar from '../../modules/NavBar/Navbar';
 import Info from '../../modules/Info/Info';
+import History from '../../modules/History/History';
 import DeckList from '../../modules/DeckList/DeckList';
 
 class Dashboard extends Component {
@@ -11,13 +12,15 @@ class Dashboard extends Component {
 		super(props);
 		this.state = {
 			statisticsData: {},
-			currentUser: {}
+			currentUser: {},
+			history: []
 		};
 	}
 
 	componentDidMount() {
 		this.getStatistics();
 		this.getCurrentUser();
+		this.getHistory();
 	}
 
 	getStatistics = async () => {
@@ -29,6 +32,18 @@ class Dashboard extends Component {
 		if (response.status === 200) {
 			const data = await response.json();
 			this.setState({ statisticsData: data, loading: false });
+		}
+	};
+
+	getHistory = async () => {
+		var url = '/api/history';
+		const token = await authService.getAccessToken();
+		const response = await fetch(url, {
+			headers: !token ? {} : { Authorization: `Bearer ${token}` }
+		});
+		if (response.status === 200) {
+			const data = await response.json();
+			this.setState({ history: data, loading: false });
 		}
 	};
 
@@ -44,16 +59,20 @@ class Dashboard extends Component {
 
 	render() {
 		let info;
+		let history;
 		const userRole = this.state.currentUser.role;
 		if (userRole != undefined && userRole === 'user') {
-			info = <Info data={this.state.statisticsData != undefined ? this.state.statisticsData : null} />
+			info = <Info data={this.state.statisticsData != undefined ? this.state.statisticsData : null} />;
+			history = <History data={this.state.history != undefined ? this.state.history : null} />;
 		}
 
 		return (
 			<div>
 				<div className="dashboard-field">
-					{info}
-
+					<div className="dashboard-container">
+						{info}
+						{history}
+					</div>
 					<DeckList menuName="Your decks" addButton="true" />
 				</div>
 			</div>
