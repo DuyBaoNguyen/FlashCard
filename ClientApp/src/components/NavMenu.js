47c0e3d151016ce8fd7450e15c3337e8563ef7cd
Duy Bao Nguyen <baobaotwd@gmail.com>
2019-12-25 21:19:49 +0700

    Add Public Cards NavBar

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -37,6 +37,9 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/cards">Cards</NavLink>
                 </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/publiccards">Public Cards</NavLink>
+                </NavItem>
                 <LoginMenu>
                 </LoginMenu>
               </ul>
