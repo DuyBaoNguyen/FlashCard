87f7bbbcaa0cae7bca0efaddbb53425bddbfdefe
Duy Bao Nguyen <baobaotwd@gmail.com>
2019-12-30 00:29:42 +0700

    Edit menubar

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LoginMenu } from './api-authorization/LoginMenu';
 import './NavMenu.css';
+import authService from './api-authorization/AuthorizeService';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      collapsed: true
+      collapsed: true,
+      user: null
     };
   }
 
-  toggleNavbar () {
+  toggleNavbar() {
     this.setState({
       collapsed: !this.state.collapsed
     });
   }
 
-  render () {
+  componentDidMount() {
+    this.populateState();
+  }
+
+  async populateState() {
+    const currentUser = await authService.getUser();
+    this.setState({
+      user: currentUser
+    });
+  }
+
+  render() {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
@@ -37,13 +50,18 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/cards">Cards</NavLink>
                 </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/publiccards">Public Cards</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/publicdecks">Public Decks</NavLink>
-                </NavItem>
-                <LoginMenu>
+                {this.state.user && this.state.user.role === 'user' ?
+                  <Fragment>
+                    <NavItem>
+                      <NavLink tag={Link} className="text-dark" to="/publiccards">Public Cards</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={Link} className="text-dark" to="/publicdecks">Public Decks</NavLink>
+                    </NavItem>
+                  </Fragment> : 
+                  ''
+                }
+                <LoginMenu user={this.state.user}>
                 </LoginMenu>
               </ul>
             </Collapse>
