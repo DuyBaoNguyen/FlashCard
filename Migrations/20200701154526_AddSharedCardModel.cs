f442c26b20152987feee858e46cb71b38cd5dd50
Duy Bao Nguyen <baobaotwd@gmail.com>
2020-07-01 22:46:37 +0700

    Add migration AddSharedCardModel

---
+++ Migrations/20200701154526_AddSharedCardModel.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlashCard.Migrations
+{
+    public partial class AddSharedCardModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "SharedDeck",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Pinned",
+                table: "SharedDeck",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Completed",
+                table: "Deck",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.CreateTable(
+                name: "SharedCard",
+                columns: table => new
+                {
+                    CardId = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    Remembered = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedCard", x => new { x.CardId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_SharedCard_Card_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Card",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SharedCard_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedCard_UserId",
+                table: "SharedCard",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SharedCard");
+
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "SharedDeck");
+
+            migrationBuilder.DropColumn(
+                name: "Pinned",
+                table: "SharedDeck");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Completed",
+                table: "Deck",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldDefaultValue: true);
+        }
+    }
+}
