b4ce82197ff0423cdc011137a33000d898e80c5b
Duy Bao Nguyen <baobaotwd@gmail.com>
2020-07-13 21:43:06 +0700

    Add swagger

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ using FlashCard.Repositories;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
 using FlashCard.Services;
+using Microsoft.OpenApi.Models;
 
 namespace FlashCard
 {
@@ -77,6 +78,11 @@ namespace FlashCard
 			{
 				configuration.RootPath = "ClientApp/build";
 			});
+
+			services.AddSwaggerGen(c => 
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -93,6 +99,12 @@ namespace FlashCard
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c => 
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+			});
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
